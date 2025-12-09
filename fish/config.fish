2e8b018b59acaf0541c85cab69df564d0d7533e8
laleks <54145@mail.ru>
2025-12-09 16:49:31 +0500

    upd

--- fish/config.fish
+++ fish/config.fish
@@ -14,17 +14,8 @@ end
 
 zoxide init --cmd cd fish | source
 
-alias getpath="find -type f | fzf | sed 's/^..//g' | tr -d '\n' | wl-copy"
-alias mf="ls | fzf -m"
-alias cppr="rsync --progress -auv"
-#alias ks="/mnt/data/lt/dotf/script/kaspro-start.sh"
-#alias kc="/mnt/data/lt/dotf/script/kaspro-coding.sh"
-#alias sotkaftp="curlftpfs ftp://sotka:say22say@192.168.0.189:2121 /home/lt/ftp/"
-#alias sotkaftp="curlftpfs 192.168.0.189:2121 /home/lt/ftp/ -o user=sotka:say22say"
-#alias usotkaftp="fusermount -u /home/lt/ftp"
+#alias getpath="find -type f | fzf | sed 's/^..//g' | tr -d '\n' | wl-copy"
+#alias mf="ls | fzf -m"
+#alias cppr="rsync --progress -auv"
 alias ls='ls -lhaXA --color=auto --group-directories-first'
 alias grep='grep --color=auto'
-#alias backup-make='/mnt/data/lt/dotf/script/backup-make.sh'
-
-
-
